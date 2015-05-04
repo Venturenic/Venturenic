0a1fdbade502b3e22df012a2586f7d02a6207346
MageBane <MageBane>
2015-05-03 19:57:38 -0500

    VTN 1.0
    VTN 1.0

---
+++ src/genesis.h
@@ -0,0 +1,13 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0x000004d97369f411d2fe58e42e7cdc1e089d40eebeb9f33f49c8ea9a858fe3ff");
+static const uint256 hashGenesisBlockTestNet("");
+
+
+
+#endif // GENESIS_H
